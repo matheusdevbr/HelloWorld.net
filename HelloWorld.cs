5f0e5d596671cd16c48fbe8141c8721ac36f366d
Matheus <76449448+matheusdevbr@users.noreply.github.com>
2021-03-03 15:57:12 -0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+
+namespace HelloWorldCSharp
+{
+    public class HelloWorld
+    {
+        private string _hello;
+
+        public void SetHello(string hello)
+        {
+            _hello = hello;
+        }
+        public string GetHello()
+        {
+            return _hello;
+        }
+    }
+}
